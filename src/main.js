f71fb1bf4047142651083bc1277c4caab30fe450
2112send <wangufovdjyu@outlook.com>
2019-04-19 19:59:23 +0800

    one

--- src/main.js
+++ src/main.js
@@ -1,7 +1,14 @@
-import Vue from "vue";
+import Vue    from "vue";
 import "./plugins/vuetify";
-import App from "./App.vue";
+import App    from "./App.vue";
 import router from "./router";
+
+// 引入 vuetify 插件
+import Vuetify from 'vuetify';
+import 'vuetify/dist/vuetify.min.css';
+
+// 使用 Vue.use 注册到全局中，来使用插件
+Vue.use(Vuetify);
 
 Vue.config.productionTip = false;
 
